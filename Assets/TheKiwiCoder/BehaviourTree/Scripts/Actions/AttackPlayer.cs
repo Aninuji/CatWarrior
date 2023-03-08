8b3d2248c69a86d9afab072011238a8cbba40d90
El Jeringas <mariost1995@hotmail.com>
2023-03-07 23:59:10 -0600

    Started Enemy Attack System

---
+++ Assets/TheKiwiCoder/BehaviourTree/Scripts/Actions/AttackPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class AttackPlayer : ActionNode
+{
+    //private float _Damage = 5;
+    private bool _didAttackEnd = false;
+    
+    protected override void OnStart()
+    {
+        context.characterActionController.Attack();
+        context.characterActionController.OnAttackEnd += AttackEnd;
+    }
+
+    protected override void OnStop()
+    {
+        context.characterActionController.OnAttackEnd -= AttackEnd;
+    }
+
+    protected override State OnUpdate()
+    {
+        if (_didAttackEnd) return State.Success;
+
+        return State.Running;
+    }
+
+    private void AttackEnd()
+    {
+        _didAttackEnd = true;
+    }
+}
